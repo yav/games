313374e6f6ddeee334aacf6bbfc5afe06483cfb6
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2017-06-10 10:26:05 -0700

    Start on state

---
+++ 51/State.hs
@@ -0,0 +1,41 @@
+module State where
+
+import Types
+
+data State = State
+  { builtLocs :: [ ActiveLoc ]
+  , dealLocs  :: [ Location ]
+
+  , handLocs  :: [ Location ]
+  , handConns :: [ Connection ]
+
+  , resources :: [Resource]
+  }
+
+data ActiveLoc = ActiveLoc
+  { actBasic  :: Location   -- ^ The static loca
+  , actAvail  :: Int
+    -- ^ How many times can this be activated.
+    -- For actiosna and open production
+  , actStored :: [(Int,Resource)]
+    -- ^ Some loctaions allow storing of items across turns
+
+  }
+
+
+hasSecretStock :: State -> Bool
+hasSecretStock = any matches . builtLocs
+  where matches l = case locationText (actBasic l) of
+                      SecretStock -> True
+                      _ -> False
+
+decreaseDefenceBy :: State -> Int
+decreaseDefenceBy = sum . map getMod . builtLocs
+  where getMod d = case locationText (actBasic d) of
+                     DecreaseDefence n -> n
+                     _ -> 0
+
+
+
+
+
