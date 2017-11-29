6e69668c10554f95a1a40c5134b587382e1c3f2d
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2017-11-28 19:22:18 -0800

    rename to "pawn" abd cleanup

---
+++ bards/Pawn.hs
@@ -0,0 +1,53 @@
+{-# Language TemplateHaskell #-}
+module Pawn where
+
+import Control.Lens(makeLenses, (^.))
+
+type PlayerId = Int
+
+data PawnId = PawnId
+  { pidOwner   :: !PlayerId
+  , pidNumber  :: !Int
+  }
+  deriving Eq
+
+data Pawn = Pawn
+  { _pawnPower      :: !Int
+  , _pawnSpeed      :: !Int
+  , _pawnId         :: !PawnId
+  , _pawnIncognito  :: !Bool
+  , _pawnBoost      :: !Int
+  } deriving Eq
+
+$(makeLenses ''Pawn)
+
+-- | A basic pawn for a player.
+pawnNew :: PawnId -> Pawn
+pawnNew pid =
+  Pawn
+    { _pawnId         = pid
+    , _pawnPower      = 1
+    , _pawnSpeed      = 1
+    , _pawnIncognito  = False
+    , _pawnBoost      = 0
+    }
+
+-- | Power levels.
+nextPowerLevel :: Int -> Int
+nextPowerLevel n
+  | n == 1    = 2
+  | n == 2    = 4
+  | otherwise = n
+
+-- | Speed levels.
+nextSpeedLevel :: Int -> Int
+nextSpeedLevel n
+  | n == 1    = 2
+  | n == 2    = 4
+  | otherwise = n
+
+pawnCurPower :: Pawn -> Int
+pawnCurPower p = (p ^. pawnPower) + (p ^. pawnBoost)
+
+
+
